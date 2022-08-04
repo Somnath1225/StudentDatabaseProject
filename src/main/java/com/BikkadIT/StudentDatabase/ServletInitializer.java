012eb0eedd374b7731e7147cd629217d3921be8a
Somnath1225 <somnath.dinde98@gmail.com>
2022-08-04 18:01:23 +0530

    ok fine thank u

---
+++ src/main/java/com/BikkadIT/StudentDatabase/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.StudentDatabase;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StudentDatabaseApplication.class);
+	}
+
+}
