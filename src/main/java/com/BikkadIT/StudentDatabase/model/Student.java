9f3dc7590a346c35b9c75a747fd6f04fff12b292
pradipwagh <pradeepwagh@9561gmail.com>
2022-08-04 18:42:57 +0530

    ok

--- src/main/java/com/BikkadIT/StudentDatabase/model/Student.java
+++ src/main/java/com/BikkadIT/StudentDatabase/model/Student.java
@@ -1,5 +1,37 @@
 package com.BikkadIT.StudentDatabase.model;
 
-public class Student {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Student {
+@Id
+	private String name;
+	private String email;
+	private Integer id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", email=" + email + ", id=" + id + "]";
+	}
+	
+	
+	
 }
